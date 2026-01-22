7df2e68274cb7a05e1bbcf802128d90b7b00d7f9
alphaMale28 <manandharsanil007@gmail.com>
2026-01-22 01:00:41 -0500

    db and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -23,10 +24,20 @@ if (ENV.NODE_ENV === "production") {
   //   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   // });
 
-  // app.get("/*any", (req, res) => {
   app.get(/.*/, (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port:", ENV.PORT),
+    );
+  } catch (error) {
+    console.error("Error starting the server", error);
+  }
+};
+
+startServer();
